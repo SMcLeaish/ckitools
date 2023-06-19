1b7002047f5214b5bcff3a46912d2b8bc2d21a0d
Sean McLeaish <smcleaish@gmail.com>
2023-06-18 20:34:22 -0400

    Map is resizable

--- src/MapBase.js
+++ src/MapBase.js
@@ -4,7 +4,8 @@ import 'leaflet/dist/leaflet.css'
 export default function MapBase(){
 
   return(
-    <div style={{ display:'flex', height:'60vh', width:'80vh', border: '2px solid black', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{ zIndex: '1', display: 'flex', justifyContent: 'center', paddingTop: '10vh' , height: '100vh', width: '100%'}}>
+      <div style={{ display:'flex',zIndex: '2',  height:'60vh', width:'70%', border: '2px solid black'}}>
       <MapContainer center={[35.07796209565608, -79.03403208754044]} zoom={13} style={{ height: "100%", width: "100%" }}> 
           <TileLayer
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -12,6 +13,7 @@ export default function MapBase(){
           <ScaleControl position="bottomleft" maxWidth={100} metric imperial />
       </MapContainer>
     </div>
+</div>
   )}
 
 
